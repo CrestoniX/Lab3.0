40361a779e1e1e0f78271577b15ee99d5e9cfbc8
CrestoniX <loltm.crav4enko@gmail.com>
2019-12-14 09:27:14 +0300

    k

--- include/SharedPtr.hpp
+++ include/SharedPtr.hpp
@@ -1,123 +1,153 @@
-#pragma once
-
 #include <atomic>
-#include <cstddef>
+#include <iostream>
 
-template<class T>
-class SharedPtr
-{
+class Control_Block {
+    std::atomic_uint counter;
 public:
-    SharedPtr()
-        : totalUse(new std::atomic<size_t>{1})
-    {}
-
-    explicit SharedPtr(T *ptr)
-        : pointer(ptr), totalUse(new std::atomic<size_t>{1})
-    {}
-
-    SharedPtr(const SharedPtr &shared)
-        : totalUse(nullptr)
-    {
-        *this = shared;
+    Control_Block() :counter(0) {}
+    void add() {
+        counter++;
     }
-
-    SharedPtr(SharedPtr &&shared) noexcept
-        : totalUse(nullptr)
-    {
-        *this = std::move(shared);
+    void del() {
+        counter--;
     }
-
-    SharedPtr &operator=(const SharedPtr &shared)
-    {
-        if (&shared == this) {
-            return *this;
-        }
-
-        this->~SharedPtr();
-
-        pointer = shared.pointer;
-        totalUse = shared.totalUse;
-        (*totalUse)++;
-
-        return *this;
+    bool empty() {
+        if (counter == 1) return true;
+        return false;
     }
-
-    SharedPtr &operator=(SharedPtr &&shared) noexcept
-    {
-        if (&shared == this) {
-            return *this;
-        }
-
-        this->~SharedPtr();
-
-        pointer = shared.pointer;
-        totalUse = shared.totalUse;
-        shared.pointer = nullptr;
-        shared.totalUse = nullptr;
-
-        return *this;
+    int value_counter() {
+        return counter;
     }
-
-    // проверяет, указывает ли указатель на объект
-    explicit operator bool() const
-    {
-        return pointer != nullptr;
-    }
-
-    T &operator*() const
-    {
-        return *pointer;
-    }
-
-    T *operator->() const
-    {
-        return pointer;
-    }
-
-    T *get()
-    {
-        return pointer;
-    }
-
-    void reset()
-    {
-        *this = SharedPtr{};
-    }
-
-    void reset(T *ptr)
-    {
-        *this = SharedPtr{ptr};
-    }
-
-    void swap(SharedPtr &shared)
-    {
-        if (&shared == this) {
-            return;
-        }
-
-        std::swap(pointer, shared.pointer);
-        std::swap(totalUse, shared.totalUse);
-    }
-
-    // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
-    [[nodiscard]] size_t use_count() const
-    {
-        return *totalUse;
-    }
-
-    ~SharedPtr()
-    {
-        if (!totalUse) {
-            return;
-        }
-
-        if (--(*totalUse) == 0) {
-            delete pointer;
-            delete totalUse;
-        }
-    }
-
-private:
-    T *pointer = nullptr;
-    std::atomic<size_t> *totalUse;
+    ~Control_Block() {};
 };
+
+template <typename T>
+class SharedPtr {
+public:
+    T* ptr;
+    Control_Block* control_block;
+    SharedPtr() :ptr(nullptr), control_block(nullptr) {};
+    SharedPtr(const SharedPtr&);
+    SharedPtr<T>& operator = (const SharedPtr<T>&);
+    SharedPtr(const T);
+
+    void reset();
+    void reset(T*);
+    void swap(SharedPtr&);
+    T* get() const;
+    T& operator*() const;
+    T* operator->() const;
+    size_t use_count() const;
+    operator bool() const;
+    ~SharedPtr();
+};
+
+template <typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr& shared_ptr) {
+    ptr = shared_ptr.get();
+    control_block = shared_ptr.control_block;
+    control_block->add();
+}
+
+template <typename T>
+SharedPtr<T>& SharedPtr<T>::operator = (const SharedPtr<T>& shared_ptr) {
+    control_block->del();
+    if (control_block->empty()) {
+        delete ptr;
+        delete control_block;
+        ptr = shared_ptr.get();
+        control_block = shared_ptr.control_block;
+        control_block->add();
+        return *this;
+    }
+    ptr = shared_ptr.get();
+    control_block = shared_ptr.control_block;
+    control_block->add();
+    return *this;
+
+}
+
+template <typename T>
+SharedPtr<T>::SharedPtr(const T value) {
+    ptr = new T(value);
+    control_block = new Control_Block();
+    control_block->add();
+}
+
+template <typename T>
+void SharedPtr<T>::reset() {
+    if (control_block->empty()) {
+        delete ptr;
+        delete control_block;
+        ptr = nullptr;
+        control_block = nullptr;
+    }
+    else {
+        ptr = nullptr;
+        control_block = nullptr;
+    }
+}
+
+template <typename T>
+void SharedPtr<T>::reset(T* curr_ptr) {
+    if (control_block->empty()) {
+        delete ptr;
+        delete control_block;
+        ptr = curr_ptr;
+        control_block = new Control_Block();
+        control_block->add();
+    }
+    else {
+        ptr = curr_ptr;
+        control_block = new Control_Block();
+        control_block->add();
+    }
+}
+
+template <typename T>
+void SharedPtr<T>::swap(SharedPtr& shared_ptr) {
+    std::swap(ptr, shared_ptr.ptr);
+    std::swap(control_block, shared_ptr.control_block);
+}
+
+template <typename T>
+T* SharedPtr<T>::get() const {
+    if (ptr == nullptr) return 0;
+    return ptr;
+}
+
+template <typename T>
+T& SharedPtr<T>::operator*() const {
+    return *ptr;
+}
+
+template <typename T>
+T* SharedPtr<T>::operator->() const {
+    return ptr;
+}
+
+template <typename T>
+size_t SharedPtr<T>::use_count() const {
+    return control_block->value_counter();
+}
+
+template <typename T>
+SharedPtr<T>::operator bool() const {
+    if (ptr == nullptr) return false;
+    return true;
+}
+
+template <typename T>
+SharedPtr<T>::~SharedPtr() {
+    if (control_block != nullptr) {
+        if (!control_block->empty()) {
+            control_block->del();
+        }
+        else {
+            delete ptr;
+            delete control_block;
+            ptr = nullptr;
+            control_block = nullptr;
+        }
+    }
+}
