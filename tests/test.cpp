bcee5381e87b278785e5860d8992a73010c9b616
CrestoniX <loltm.crav4enko@gmail.com>
2019-12-22 23:42:14 +0300

    ddad

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,20 +1,7 @@
-"Copyright [2019] <Copyright owner>"
+// Copyright 2019 CrestoniX <your_email>
+
 #include <gtest/gtest.h>
 
-#include "SharedPtr.hpp"
-#include <string>
-
-TEST(SharedPtr, End)
-{
-    auto result1 = counter--;
-    auto result2 = counter++;
-    EXPECT_EQ(ptr, nullptr);
-    EXPECT_EQ(control_block, nullptr);
-    EXPECT_EQ(control_block->del(), result1);
-    EXPECT_EQ(control_block->add(), result2);
+TEST(Example, EmptyTest) {
+EXPECT_TRUE(true);
 }
-
-
-
-
-
