989bd941f68596ae5d17df6031e1c934fe796d44
CrestoniX <loltm.crav4enko@gmail.com>
2019-12-22 22:37:12 +0300

    ddad

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,66 +3,13 @@
 #include "SharedPtr.hpp"
 #include <string>
 
-
-TEST(SharedPtr, FromPointer)
+TEST_F(SharedPtr, EndOfProgramm)
 {
+    EXCEPT_EQ(ptr, nullptr);
+    EXCEPT_EQ(control_block, nullptr);
 
 }
 
-TEST_F(SharedPtrFixture, Empty)
-{
-
-}
-
-TEST_F(SharedPtrFixture, FromPointerString)
-{
-
-}
-
-TEST_F(SharedPtrFixture, CopyConstructor)
-{
-
-}
-
-TEST_F(SharedPtrFixture, MoveConstructor)
-{
-
-}
-
-TEST_F(SharedPtrFixture, AssignmentCopy)
-{
-
-}
-
-TEST_F(SharedPtrFixture, AssignmentCopySelf)
-{
-
-}
-
-TEST_F(SharedPtrFixture, AssignmentMove)
-{
-
-}
-
-TEST_F(SharedPtrFixture, reset)
-{
-
-}
-
-TEST_F(SharedPtrFixture, ResetWithPointer)
-{
-
-}
-
-TEST_F(SharedPtrFixture, ResetWithPointerString)
-{
-
-}
-
-TEST_F(SharedPtrFixture, Swap)
-{
-
-}
 
 
 
