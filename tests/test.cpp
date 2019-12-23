3b247f473d3def390c28e34474e8022c5d3b4129
CrestoniX <loltm.crav4enko@gmail.com>
2019-12-23 21:20:00 +0300

    g

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,8 @@
 // Copyright 2019 CrestoniX <loltm.crav4enko@gmail.com>
 #include <gtest/gtest.h>
 
-TEST(Control_Block, EmptyTest) {
-EXPECT_EQ(counter, 0);
+TEST(Control_Block, Test) {
+    EXPECT_EQ(counter, 0);
 }
 TEST(SharedPtr, Tests) {
     EXPECT_EQ(ptr, nullptr);
